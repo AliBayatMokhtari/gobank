aecb3a133b87d17058932093cbcbd72e5a0a5822
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2026-02-26 12:54:35 +0330

    initial http server to serve Hello World [WIP]

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
 
 func main() {
-	fmt.Println("Hello from GoBank")
+	router := chi.NewMux()
+
+	router.Get("/", handleIndex)
+
+	http.ListenAndServe(":8080", router)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
 }
