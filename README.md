e0349c0b20fdd8b1e8100ae8c7a5a79695cb953d
Ali Bayat Mokhtari <ali.bayatmokhtari96@gmail.com>
2026-02-27 00:04:14 +0330

    init README.md file [FEAT]

---
+++ README.md
@@ -0,0 +1,17 @@
+# Core features
+
+- [ ] Authentication
+- [ ] Create wallet for user when user's national code is verified
+- [ ] User can charge their wallet
+- [ ] User can cash out from their wallet (Deposit to bank account)
+- [ ] User can transfer credit to other user wallet (With wallet public id)
+- [ ] User should see list of transactions in details
+  - Transfer date
+  - Destination wallet's owner name
+  - Transfer amount
+  - Transfer status
+- [ ] User can filter their transactions based on date range or transaction status
+
+# Extra feature
+
+- [ ] User can export report for hist wallet in csv format
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -10,10 +12,62 @@ func main() {
 	router := chi.NewMux()
 
 	router.Get("/", handleIndex)
+	router.Get("/user/{id}", HandleGetUser)
 
 	http.ListenAndServe(":8080", router)
 }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World"))
+	user := User{
+		ID:       1,
+		Name:     "John Doe",
+		Username: "ali_bm",
+	}
+
+	data, err := json.Marshal(user)
+
+	if err != nil {
+		http.Error(w, "Failed to marshal user data", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(data)
+}
+
+func HandleGetUser(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	idInt, err := strconv.Atoi(id)
+
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+
+	user := User{
+		ID:       idInt,
+		Name:     "John Doe",
+		Username: "ali_bm",
+	}
+
+	data, err := json.Marshal(user)
+
+	if err != nil {
+		http.Error(w, "Failed to marshal user data", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(data)
+}
+
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"user_name"`
 }
